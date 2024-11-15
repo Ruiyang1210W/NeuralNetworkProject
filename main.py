1ef19d08d85fd08207282071ef76d654246f81fc
[Ruiyang Wang] <[wangruiyang1210@gmail.com]>
2024-11-14 19:54:28 -0800

    Add headers for main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Main script for running the net
+import numpy as np
+from Model import Model
+from Layers import Layer, InputLayer
+from Activations import ReLU, Activation
+
+#Load and Preprocess Data
+
+
+# Initialize the Model
+
+
+# Define the Loss Function
+
+
+# Set Up the Training Loop
+
+
+# Evaluate the Model
